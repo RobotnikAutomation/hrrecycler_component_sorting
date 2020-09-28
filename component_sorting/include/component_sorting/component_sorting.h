37aae64e3c88ab7ff0817430dc7cd4db7c8bb1f2
marbosjo <mbosch@robotnik.es>
2020-09-28 17:30:19 +0200

    component_sorting: connects to server

--- component_sorting/include/component_sorting/component_sorting.h
+++ component_sorting/include/component_sorting/component_sorting.h
@@ -5,14 +5,19 @@
 
 #include <rcomponent/rcomponent.h>
 
+#include <actionlib/server/simple_action_server.h>
+#include <actionlib/server/simple_action_server.h>
+
 #include <moveit/move_group_interface/move_group_interface.h>
+
+#include <moveit_msgs/PickupAction.h>
+#include <moveit_msgs/PlaceAction.h>
 
 class ComponentSorting : public rcomponent::RComponent
 {
 public:
   ComponentSorting(ros::NodeHandle h);
   ~ComponentSorting();
-
 
 protected:
   // RComponent stuff
@@ -24,16 +29,28 @@ protected:
   //! Reads params from params server
   virtual void rosReadParams();
 
+  virtual int setup();
+
   // States
   virtual void standbyState();
   virtual void readyState();
 
   // ROS stuff
   std::shared_ptr<tf2_ros::Buffer> tf2_buffer_;
+
   // MoveIt stuff
   std::string group_name_;
   ros::WallDuration move_group_timeout_;
   moveit::planning_interface::MoveGroupInterfacePtr move_group_;
+
+  std::shared_ptr<actionlib::SimpleActionServer<moveit_msgs::PickupAction>> pickup_as_;
+  std::shared_ptr<actionlib::SimpleActionServer<moveit_msgs::PlaceAction>> place_as_;
+
+  std::shared_ptr<actionlib::SimpleActionClient<moveit_msgs::PickupAction>> pickup_ac_;
+  std::shared_ptr<actionlib::SimpleActionClient<moveit_msgs::PlaceAction>> place_ac_;
+
+  virtual void goalCB();
+  virtual void preemptCB();
 };
 
 #endif  // _COMPONENT_SORTING__COMPONENT_SORTING_H_
--- component_sorting/src/component_sorting.cpp
+++ component_sorting/src/component_sorting.cpp
@@ -31,18 +31,31 @@ int ComponentSorting::rosSetup()
     return rcomponent::INITIALIZED;
   }
 
-  tf2_buffer_.reset(new tf2_ros::Buffer); 
+  tf2_buffer_.reset(new tf2_ros::Buffer);
   try
   {
-    move_group_.reset(new moveit::planning_interface::MoveGroupInterface(group_name_, 
-    tf2_buffer_, move_group_timeout_));
+    move_group_.reset(
+        new moveit::planning_interface::MoveGroupInterface(group_name_, tf2_buffer_, move_group_timeout_));
   }
-  catch (const std::runtime_error& e) 
+  catch (const std::runtime_error& e)
   {
-    RCOMPONENT_ERROR("Cannot create move group with group name: %s. Is MoveIt running? Group name is correct?", group_name_.c_str());
+    RCOMPONENT_ERROR("Cannot create move group with group name: %s. Is MoveIt running? Group name is correct?",
+                     group_name_.c_str());
     RCOMPONENT_ERROR_STREAM("Exception: " << e.what());
     return rcomponent::ERROR;
   }
+
+  bool autostart = false;
+  pickup_as_.reset(new actionlib::SimpleActionServer<moveit_msgs::PickupAction>(pnh_, "pickup", autostart));
+  pickup_as_->registerGoalCallback(boost::bind(&ComponentSorting::goalCB, this));
+  pickup_as_->registerPreemptCallback(boost::bind(&ComponentSorting::preemptCB, this));
+  place_as_.reset(new actionlib::SimpleActionServer<moveit_msgs::PlaceAction>(pnh_, "place", autostart));
+  place_as_->registerGoalCallback(boost::bind(&ComponentSorting::goalCB, this));
+  place_as_->registerPreemptCallback(boost::bind(&ComponentSorting::preemptCB, this));
+
+  bool spin_action_thread = true;
+  pickup_ac_.reset(new actionlib::SimpleActionClient<moveit_msgs::PickupAction>(nh_, "pickup", spin_action_thread));
+  place_ac_.reset(new actionlib::SimpleActionClient<moveit_msgs::PlaceAction>(nh_, "place", spin_action_thread));
 
   return RComponent::rosSetup();
 }
@@ -52,11 +65,51 @@ int ComponentSorting::rosShutdown()
   return RComponent::rosShutdown();
 }
 
+int ComponentSorting::setup()
+{
+  // Checks if has been initialized
+  int setup_result;
+
+  setup_result = rcomponent::RComponent::setup();
+  if (setup_result != rcomponent::OK)
+  {
+    return setup_result;
+  }
+
+  ros::Duration timeout = ros::Duration(5);
+  pickup_ac_->waitForServer(timeout);
+  if (pickup_ac_->isServerConnected() == false)
+  {
+    RCOMPONENT_ERROR_STREAM("Cannot connect to pick up client: " << "pickup");
+    return rcomponent::ERROR;
+  }
+  place_ac_->waitForServer(timeout);
+  if (place_ac_->isServerConnected() == false)
+  {
+    RCOMPONENT_ERROR_STREAM("Cannot connect to pick up client: " << "place");
+    return rcomponent::ERROR;
+  }
+  
+  pickup_as_->start();
+  RCOMPONENT_INFO_STREAM("Started server: " << "pickup");
+  place_as_->start();
+  RCOMPONENT_INFO_STREAM("Started server: " << "place");
+}
+
 void ComponentSorting::standbyState()
 {
   switchToState(robotnik_msgs::State::READY_STATE);
 }
 
 void ComponentSorting::readyState()
+{
+}
+
+void ComponentSorting::goalCB()
+{
+
+}
+
+void ComponentSorting::preemptCB()
 {
 }
